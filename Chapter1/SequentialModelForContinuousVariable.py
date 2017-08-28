b1cc83dca9ff057d41783a24980197c029f70f43
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-08-28 13:26:38 -0400

    Update and rename simpleSequentialModel.py to SequentialModelForContinuousVariable.py

--- Chapter1/SequentialModelForContinuousVariable.py
+++ Chapter1/SequentialModelForContinuousVariable.py
@@ -5,7 +5,7 @@ model=Sequential()
 from keras.layers import Dense, Activation
 
 # add dense layer. In dense layer, each neuron is connected to all the neurons in next layer
-model.add(Dense(units=64, input_dim=100))
+model.add(Dense(units=64, input_dim=5))
 
 # add activation function for each layer. Each layer has its activation function. It represents the function 
 # applied on the output of the neuron
@@ -17,11 +17,23 @@ model.add(Dense(units=10))
 # add a activation function
 model.add(Activation('softmax'))
 
+
+# Similarly, lets add one more layer with 1 neuron
+model.add(Dense(units=1))
+
+# add a activation function
+model.add(Activation('softmax'))
+
+
 # compile the above model of 2 layers
-model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy']) 
+model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy']) 
 
 # Now we need to create a numpy matrix x_train and a numpy array y_train
 
+import numpy as np
+
+x_train=np.random.rand(100,5)
+y_train=np.random.rand(100,)
 
 model.fit(x_train, y_train, epochs=5, batch_size=32)
 
