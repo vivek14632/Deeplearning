12d0420a26786099c12ddd39f836626fd0ae1238
vivek14632 <vksingh.iiitb@gmail.com>
2017-11-04 16:06:29 -0400

    code cleaning

--- chapter3/Cats_vs_Dog_test.py
+++ chapter3/Cats_vs_Dog_test.py
@@ -1,28 +1,20 @@
-
-# coding: utf-8
-
-# In[4]:
-
+# Vivek: What is the use of ImageDataGenerator?
 from keras.preprocessing.image import ImageDataGenerator
 
+# Vivek: Are there models other than sequential?
 from keras.models import Sequential
 
 from keras.layers import Conv2D, MaxPooling2D
 
 from keras.layers import Activation, Dropout, Flatten, Dense
 
+# Vivek: What is the purpose of backend?
 from keras import backend as K
 
-
-
-
-
 # dimensions of our images.
-
 img_width, img_height = 150, 150
 
-
-
+# Vivek: Any public link for these images so that others can download?
 train_data_dir = 'C:/Users/preks/Documents/data/train'
 
 validation_data_dir = 'C:/Users/preks/Documents/data/validation'
@@ -31,79 +23,74 @@ nb_train_samples = 2000
 
 nb_validation_samples = 800
 
+# Vivek: what is the importance of epochs and batch_size?
 epochs = 25
-
 batch_size = 16
 
 
-
+# Vivek: Not clear what exactly are we doing here?
+# Vivek: What is 'channels_first'? It seems like whether the color is x dimension 
+# or z dimension?
 if K.image_data_format() == 'channels_first':
-
-    input_shape = (3, img_width, img_height)
-
+	input_shape = (3, img_width, img_height)
 else:
-
-    input_shape = (img_width, img_height, 3)
-
-
+	input_shape = (img_width, img_height, 3)
 
 model = Sequential()
 
+# Convolution layer with 32 units.
 model.add(Conv2D(32, (3, 3), input_shape=input_shape))
 
+# RELU activation
 model.add(Activation('relu'))
 
+# Max pooling
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-
-
+# 2nd convolution layer
 model.add(Conv2D(32, (3, 3)))
 
+# 2nd activation layer
 model.add(Activation('relu'))
 
+# 2nd max pooling layer
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-
-
+# Third convolution layer. 
+# Vivek: why did we increase the units to 64? The earlier convolution layers 
+# have 32 units
 model.add(Conv2D(64, (3, 3)))
 
+# Third activation layer
 model.add(Activation('relu'))
 
+# Third pooling layer
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-
-
+# Vivek: What is the purpose of flatten layer?
 model.add(Flatten())
 
+# Dense layer
 model.add(Dense(64))
 
+# Final relu activation
 model.add(Activation('relu'))
 
+# Dropout layer to reduce overfitting
 model.add(Dropout(0.5))
 
+# Output layer, since its just 2 classes so one neuron is enought (0/1)
 model.add(Dense(1))
 
+# sigmoid activation similar to a logistic regression
 model.add(Activation('sigmoid'))
 
-
-
-model.compile(loss='binary_crossentropy',
-
-              optimizer='rmsprop',
-
-              metrics=['accuracy'])
-
-
+# Compile the above model 
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
 # this is the augmentation configuration we will use for training
 
-train_datagen = ImageDataGenerator(
-
-    rescale=1. / 255,
-
-    shear_range=0.2,
-
-    zoom_range=0.2,
+train_datagen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2, zoom_range=0.2,
     rotation_range=20,# angle at which the image is rotated
 width_shift_range=0.5, # randomly translate as a fraction of total width or height
 height_shift_range=0.5,
@@ -216,49 +203,3 @@ preds = test_model.predict_classes(x)
 
 prob = test_model.predict_proba(x)
 print(preds, prob)
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
